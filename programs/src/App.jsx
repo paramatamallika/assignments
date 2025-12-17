8fe598a3ccc1346b7eed513d62932d09d84ea118
paramatamallika <paramatamallika@gmail.com>
2025-12-17 11:16:47 +0530

    programs

--- programs/src/App.jsx
+++ programs/src/App.jsx
@@ -1,12 +1,19 @@
 import Counter from "./Counter";
+import Calculator from "./Calculator";
 
 function App() {
   return (
     <div>
       <h2>Counter App</h2>
       <Counter />
+
+      <hr />
+
+      <h2>Calculator App</h2>
+      <Calculator />
     </div>
   );
 }
 
 export default App;
+
