48095e2b51cda85acad7183fa694b721112d30e2
paramatamallika <paramatamallika@gmail.com>
2025-12-29 18:17:07 +0530

    Axios

---
+++ Axios/src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import TodoList from "./pages/TodoList";
+import TodoDetails from "./pages/TodoDetails";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<TodoList />} />
+      <Route path="/todo/:id" element={<TodoDetails />} />
+    </Routes>
+  );
+}
